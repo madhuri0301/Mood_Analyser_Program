d478c35f0be1541ba61d574e5b9a967a006d43ee
Madhuri <harshupatil0301@gmail>
2021-06-30 11:46:15 +0530

    [Madhuri]Add : Welcome Message Uploaded

---
+++ Mood_Analyser_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mood_Analyser_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Mood Analyser Program");
+        }
+    }
+}
